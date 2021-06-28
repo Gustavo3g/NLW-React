da59652f4e0d260a3304788ba0773fef60aaf143
gustavo3g <gustavo3g@github.com>
2021-06-28 09:16:07 -0300

    Implementaçao da autenticaçao firebase

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
+import { ButtonHTMLAttributes } from 'react'
 
-export function Button() {
+import '../styles/button.scss';
 
-    const [counter, setCouter] = useState(0);
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-    function increment (){
-        setCouter(counter + 1);
-
-    }
-
-
+export function Button(props : ButtonProps) {
 
     return (
-        <button onClick={increment}>{counter}</button>
-    );
+        <button className="button" {...props}></button>
+    )
 }
